c3f3d9bbea97cb0af9dcab9484691d26513eadc5
tje9 <tje9@students.calvin.edu>
2016-12-18 12:40:23 -0500

    About Page

--- app/scripts/About/About.js
+++ app/scripts/About/About.js
@@ -12,7 +12,11 @@ constructor(props) {
                 console.log(this.state.signedIn);
         return (
             <div className="about-page">
-                <p> This is the about page </p>
+                <h1>Media Catalog</h1>
+<br></br>
+    <h3>Media Catalog is a web application that caters towards users that want a centralized place to store records of their music, books or movies. No only that, but it is also a hub to post comments about media and have discussions with others. We have utlized google logins so that users can distinguish who posted what and have private catalogs.</h3>
+<br></br>
+<h4>It was created by Kristofer Brink and Trevor Edewaard for their web development class at Calvin College.</h4>
             </div>
             );
     }
