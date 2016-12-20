e95f53f812cdbece53573c6a06dba8f57af714d6
Krisofer Brink <kpgbrink@gmail.com>
2016-12-19 20:51:02 -0500

    added footer. sidebar is not needed for this type of site.

--- app/scripts/AppFrame.js
+++ app/scripts/AppFrame.js
@@ -4,7 +4,7 @@ import { IndexLink, Link} from 'react-router';
 
 /*
 *Static Frame that is always present.
-*Includes the Header and link to other components
+*Includes the Header, Footer, and link to other components
 */
 
 export default class extends React.Component{
@@ -18,25 +18,28 @@ export default class extends React.Component{
     render() {
         console.log('rendering AppFrame')
         return (
-            <div>
-                <div className="header-title">
-                    <h2 className="header-title-text">Media React</h2>
-                </div>
-                <ul className="header">
-                    <div className="header-left">
-                        <img className="media-react-image" alt="Media React" src="/images/mediaReact.png"/>
-                        <div className="links">
-                            <li><IndexLink to="/Post" activeClassName="active">Post</IndexLink></li>
-                            <li><Link to="/Catalog" activeClassName="active">Catalog</Link></li>
-                            <li><Link to="/About" activeClassName="active">About</Link></li>
-                            <li><Link to="/MyAccount" activeClassName="active">My Account</Link></li>
-                        </div>
+            <div className="app-frame">
+                <div>
+                    <div className="header-title">
+                        <h2 className="header-title-text">Media React</h2>
                     </div>
-                    <User onSignIn={this.props.onSignIn} onSignOut={this.props.onSignOut} user={this.props.user}/>
-                </ul>
-                <div className="content">
-                    {this.props.children}
+                    <ul className="header">
+                        <div className="header-left">
+                            <img className="media-react-image" alt="Media React" src="/images/mediaReact.png"/>
+                            <div className="links">
+                                <li><IndexLink to="/Post" activeClassName="active">Post</IndexLink></li>
+                                <li><Link to="/Catalog" activeClassName="active">Catalog</Link></li>
+                                <li><Link to="/About" activeClassName="active">About</Link></li>
+                                <li><Link to="/MyAccount" activeClassName="active">My Account</Link></li>
+                            </div>
+                        </div>
+                        <User onSignIn={this.props.onSignIn} onSignOut={this.props.onSignOut} user={this.props.user}/>
+                    </ul>
+                    <div className="content">
+                        {this.props.children}
+                    </div>
                 </div>
+                <div className="footer"/>
             </div>
         );
     }
